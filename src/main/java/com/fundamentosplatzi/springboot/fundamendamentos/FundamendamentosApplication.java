10e7c4077958d8a8026f0ba5e181a13de5a3a65a
Luis Santiago <lsantiago@mx.fimex.com>
2023-04-07 16:35:02 -0600

    first commit

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamendamentos/FundamendamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.fundamendamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamendamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamendamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi/springboot/fundamendamentos/FundamendamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.fundamendamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamendamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
